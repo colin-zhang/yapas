6b8e316c97a33be75ea904e12f484be0adef4321
colin-zhang <ingm@foxmail.com>
2017-01-13 16:33:16 +0800

    a

--- dpdk/sys_init.sh
+++ dpdk/sys_init.sh
@@ -9,11 +9,11 @@ set -e
 #apt install oprofile
 #
 
-echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+#echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
 #On a NUMA machine, pages should be allocated explicitly on separate nodes:
-#echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-#echo 1024 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+echo 2048 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+echo 2048 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
 if [[ ! -d /mnt/huge ]]; then
     mkdir /mnt/huge
 fi
@@ -22,3 +22,10 @@ mount -t hugetlbfs nodev /mnt/huge
 
 #sudo modprobe uio
 #sudo insmod kmod/igb_uio.ko
+sudo modprobe uio
+sudo rmmod igb_uio.ko
+sudo insmod ${DVLP_}/lib/modules/$(uname -r)/extra/dpdk/igb_uio.ko
+
+dpdk-devbind --bind=igb_uio $1
+
+echo 1
--- pre/pre.sh
+++ pre/pre.sh
@@ -1,3 +1,7 @@
 yum install libunwind-devel
 wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.5/gperftools-2.5.tar.gz
 
+wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.5/gperftools-2.5.tar.gz
+
+wget http://prdownloads.sourceforge.net/scons/scons-2.5.1.tar.gz
+
