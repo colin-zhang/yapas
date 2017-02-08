0800568299b982cd295c01dc402262a8b6fca451
colin-zhang <ingm@foxmail.com>
2017-02-08 22:20:20 +0800

    xx

--- common/readline.cc
+++ common/readline.cc
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <termios.h>
 #include <string.h>
+#include <fcntl.h>
 #include <string>
 #include <vector>
 
@@ -21,7 +22,7 @@ public:
     void clearLine();
     void writeLine(string &buf);
 private:
-    struct termios tty_atrr;
+    struct termios old_tty_atrr;
     vector <string> history; 
     vector<string>::iterator curr;
     string prompt;
@@ -45,11 +46,14 @@ CmdLine::CmdLine(const char *prompt_cstr)
     out_fd = STDOUT_FILENO;
     prompt = string(prompt_cstr);
     init();
+    //int flags = fcntl(in_fd, F_GETFL, 0);  
+    //fcntl(in_fd, F_SETFL, flags | O_NONBLOCK);
+
 }
 
 CmdLine::~CmdLine()
 {
-
+    tcsetattr(in_fd, TCSANOW, &old_tty_atrr);
 }
 
 void CmdLine::clearLine()
@@ -67,6 +71,7 @@ void CmdLine::init()
 {
     struct termios tty_attr;
     tcgetattr(in_fd, &tty_attr);
+    old_tty_atrr = tty_attr;
     tty_attr.c_lflag &= (~(ICANON|ECHO));
     tty_attr.c_cc[VTIME] = 0;
     tty_attr.c_cc[VMIN] = 1;
@@ -117,7 +122,7 @@ string CmdLine::readLine()
     } else if (c == 0x1B) {
        char b[2];
        int len = read(in_fd, b, sizeof(b));
-       if (b[0] == 0x5B && b[1] == 0x41) {
+       if (b[0] == 0x5B && b[1] == 0x41 && history.size() > 0) {
             //up arrow
             clearLine();
             writeLine(*curr);
@@ -126,7 +131,7 @@ string CmdLine::readLine()
             } else {
                 curr--;
             }
-       } else  if (b[0] == 0x5B && b[1] == 0x42) {
+       } else  if (b[0] == 0x5B && b[1] == 0x42 && history.size() > 0) {
             //down arrow
             if (curr == history.end() - 1) {
             } else {
@@ -163,6 +168,9 @@ int main()
             //std::cout << c << std::endl;
             printf("%s \n", c.c_str());
             fflush(stdout);
+            if (c == "q") {
+                break;
+            }
         }
     }
 
