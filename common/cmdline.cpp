14e22f45419476b6f3d7619486b7ed90933530b8
root <root@localhost.localdomain>
2017-02-22 20:56:33 +0800

    cmd

---
+++ common/cmdline.cpp
@@ -0,0 +1,202 @@
+#include "access_cmdline.h"
+
+using namespace std;
+
+CmdLine::CmdLine()
+{
+    in_fd = STDIN_FILENO;
+    out_fd = STDOUT_FILENO;
+    prompt = string("> ");
+    init();
+}
+
+CmdLine::CmdLine(const char *prompt_cstr)
+{
+    in_fd = STDIN_FILENO;
+    out_fd = STDOUT_FILENO;
+    prompt = string(prompt_cstr);
+    init();
+    //int flags = fcntl(in_fd, F_GETFL, 0);  
+    //fcntl(in_fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+CmdLine::~CmdLine()
+{
+    tcsetattr(in_fd, TCSANOW, &old_tty_atrr);
+}
+
+void CmdLine::addHints(string& hint)
+{
+    std::vector<string>::iterator it;
+    for (it = hints.begin(); it != hints.end(); it++) {
+        if (*it == hint) {
+            break;
+        }
+    }
+    if (it == hints.end()) {
+        hints.push_back(hint);
+    }
+    //hints.insert(hint);
+}
+
+void CmdLine::clearLine()
+{
+    backSpace(cmd.size());
+}
+
+void CmdLine::writeLine(string& buf)
+{
+    write(out_fd, buf.c_str(), buf.size());
+    //fsync(out_fd);
+}
+
+void CmdLine::init()
+{
+    struct termios tty_attr;
+    tcgetattr(in_fd, &tty_attr);
+    old_tty_atrr = tty_attr;
+    tty_attr.c_lflag &= (~(ICANON|ECHO));
+    tty_attr.c_cc[VTIME] = 0;
+    tty_attr.c_cc[VMIN] = 1;
+    tcsetattr(in_fd, TCSANOW, &tty_attr);
+    writeLine(prompt);
+}
+
+void CmdLine::backSpace(int n) 
+{
+    char *s = new char[n];
+    char *b = new char[n];
+    memset(s, ' ', n);
+    memset(b, '\b', n);
+    write(out_fd, b, n);
+    write(out_fd, s, n);
+    write(out_fd, b, n);
+    delete []s;
+    delete []b;
+}
+
+int  CmdLine::compareHints(std::string* possible)
+{   
+    int most_len = 65535;
+    std::vector<string> common;
+    std::vector<string>::iterator it = hints.begin();
+    possible->clear();
+    for ( ; it != hints.end(); it++) {
+        if (it->length() > cmd.length()) {
+            if (0 == it->compare(0, cmd.length(), cmd)) {
+                common.push_back(*it);
+                possible->append(*it);
+                possible->append(" ");
+                if (it->length() < most_len) {
+                    most_len = it->length();
+                }
+            }
+        }
+    }
+    if (common.size() < 2) {
+        return common.size();
+    }
+
+    string common_str("");
+    char x = 0, xx = 0;
+    for (int i = cmd.length(); i < most_len; i++) {
+        it = common.begin();
+        x = (*it)[i];
+        for ( ; it != common.end(); it++) {
+            xx = (*it)[i];
+            if (xx != x) {
+                x = 0;
+                break;
+            }
+        }
+        if (x) {
+            common_str.append(1, x);
+        } else {
+            break;
+        }
+    }
+    cmd.append(common_str);
+    return common.size();
+}
+
+string CmdLine::readLine()
+{
+    string res("");
+    int len = read(in_fd, &c, 1); 
+
+    if (c == '\n') {
+        write(out_fd, &c, 1);
+        if (cmd.empty()) {
+            writeLine(prompt);
+        } else {
+            history.push_back(cmd);
+            curr = history.end() - 1;
+        }
+        res = string(cmd);
+        cmd.clear();
+    } else if (c == '\t') {
+        int count = 0;
+        string possible_cmd;
+        count = compareHints(&possible_cmd);
+        if (count > 1 && last_key == c && last_time == time(NULL)) {
+            possible_cmd.insert(0, "\n");
+            possible_cmd.append("\n");
+            writeLine(possible_cmd);
+            writeLine(prompt);
+            writeLine(cmd);
+            //cmd.clear();
+        } else if (1 == count) {
+            clearLine();
+            writeLine(possible_cmd);
+            cmd = possible_cmd;
+        }
+        //set<string, HintCompare>::iterator it = hints.find(cmd);
+/*        if (it != hints.end()) {
+            //if (it->length() > cmd.length()) {
+                clearLine();
+                writeLine(*it);
+                cmd = *it;
+            //}
+        }*/
+    } else if (c == 127 || c == 8) {
+        if (!cmd.empty()) {
+            cmd.erase(cmd.end() - 1);
+            backSpace(1);
+        }
+    } else if (c == 0x1B) {
+       char b[2];
+       int len = read(in_fd, b, sizeof(b));
+       if (b[0] == 0x5B && b[1] == 0x41 && history.size() > 0) {
+            //up arrow
+            clearLine();
+            writeLine(*curr);
+            cmd = string(*curr);
+            if (curr == history.begin()) {
+            } else {
+                curr--;
+            }
+       } else  if (b[0] == 0x5B && b[1] == 0x42 && history.size() > 0) {
+            //down arrow
+            if (curr == history.end() - 1) {
+            } else {
+                curr++;
+                clearLine();
+                writeLine(*curr);
+                cmd = string(*curr);
+            }
+       } else {
+            //printf("\n%02x, %02x", b[0], b[1]);
+            //fflush(stdout);
+       }
+    } else if (isprint(c)) {
+        write(out_fd, &c, 1);
+        cmd.push_back(c);
+    } else {
+        //printf("\n%02x", c);
+        //fflush(stdout);
+    }
+    last_key = c;
+    last_time = time(NULL);
+    //fsync(out_fd);
+    return res;
+}
---
+++ common/cmdline.h
@@ -0,0 +1,56 @@
+#ifndef _CQMS_CMD_LINE_H
+#define _CQMS_CMD_LINE_H
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <termios.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <set>
+
+/*class HintCompare 
+{
+public:
+    bool operator() (const string &a, const string &b) const {
+        //int n = b.size() > a.size() ? a.size() : b.size();
+        int n = a.size();
+        printf("\n%s\n", a.c_str());
+        if (strncmp(a.c_str(), b.c_str(), n) < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};*/
+
+class CmdLine
+{
+public:
+    CmdLine();
+    CmdLine(const char *prompt_cstr);
+    ~CmdLine();
+    void init();
+    std::string readLine();
+    void backSpace(int n);
+    void clearLine();
+    void writeLine(std::string& buf);
+    void addHints(std::string& hint);
+    int  compareHints(std::string* possible);
+private:
+    struct termios old_tty_atrr;
+    std::vector <std::string> history; 
+    std::vector<std::string>::iterator curr;
+    //set<string, HintCompare> hints;
+    std::vector<std::string> hints;
+    std::string prompt;
+    std::string cmd;
+    char c;
+    int in_fd;
+    int out_fd;
+    char last_key;
+    int  last_time;
+};
+
+#endif
